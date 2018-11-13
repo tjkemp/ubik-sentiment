3765b3d3e846177cd753b99e1cf446a659e119ad
tjkemp <tero.kemppi@gmail.com>
2018-11-13 19:41:39 +0200

    improved readability

--- data/generate_data.sh
+++ data/generate_data.sh
@@ -3,22 +3,26 @@
 # Uncomment to fetch original data
 #python ./korp_api.py > korp_lauseet.txt
 
-cat korp_lauseet.txt | grep -Pv "^:\)$|^:\($" > deleteme_clean.txt
-cat deleteme_clean.txt | shuf > deleteme_shuffle.txt
+# 1. extract data with either :) or :( smiley
+cat korp_lauseet.txt | grep -Pv "^:\)$|^:\($" > temp_clean.txt
 
-# divide it to train and test sets
-len_total=$(wc -l deleteme_shuffle.txt| awk '{print $1}')
-len_testset=$(expr $len_total / 5)
+# 2. shuffle the data
+cat temp_clean.txt | shuf > temp_shuffle.txt
+
+# 3. divide the data to training set (2/3) and test set (1/3)
+len_total=$(wc -l temp_shuffle.txt| awk '{print $1}')
+
+len_testset=$(expr $len_total / 3)
 len_trainset=$(expr $len_total - $len_testset)
 
-cat deleteme_shuffle.txt | head -$len_trainset > deleteme_train.txt
-cat deleteme_shuffle.txt | tail -$len_testset > deleteme_test.txt
+cat temp_shuffle.txt | head -$len_trainset > temp_train.txt
+cat temp_shuffle.txt | tail -$len_testset > temp_test.txt
 
-# divide sets into negative and positive sentiment sets
-cat deleteme_train.txt |grep ":)"|perl -pe 's/\:\)//g' >  korp_train_pos.txt
-cat deleteme_train.txt |grep ":("|perl -pe 's/\:\(//g' >  korp_train_neg.txt
-cat deleteme_test.txt |grep ":)"|perl -pe 's/\:\)//g' >  korp_test_pos.txt
-cat deleteme_test.txt |grep ":("|perl -pe 's/\:\(//g' >  korp_test_neg.txt
+# 4. remove smileys and divide sets into negative and positive sentiment sets
+cat temp_train.txt |grep ":)"|perl -pe 's/\:\)//g' >  korp_train_pos.txt
+cat temp_train.txt |grep ":("|perl -pe 's/\:\(//g' >  korp_train_neg.txt
+cat temp_test.txt |grep ":)"|perl -pe 's/\:\)//g' >  korp_test_pos.txt
+cat temp_test.txt |grep ":("|perl -pe 's/\:\(//g' >  korp_test_neg.txt
 
-# clean up
-rm deleteme_*.txt
+# delete temporary files
+rm temp_*.txt
