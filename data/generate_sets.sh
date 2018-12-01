2856f5a6767ba0cf50d4bdea9c8389668bc8c158
tjkemp <tero.kemppi@gmail.com>
2018-12-01 09:15:23 +0200

    creates eval set and uses temp files

--- data/generate_sets.sh
+++ data/generate_sets.sh
@@ -1,31 +1,50 @@
 #!/bin/sh
 
-# Uncomment to fetch original data
-#python ./korp_api.py > korp_lauseet.txt
+# This shell scripts cleans a little bit the raw smiley data got with: 
+# python ./fetch_rawdata.py > korp_all_sentences.txt
+# and then creates training and development sets and eval file for annotation
 
-# 1. extract data with either :) or :( smiley
-cat korp_lauseet.txt | grep -Pv "^:\)$|^:\($" > temp_clean.txt
+tempfile_clean=$(mktemp)
+tempfile_train=$(mktemp)
+tempfile_half=$(mktemp)
+tempfile_devel=$(mktemp)
+tempfile_eval=$(mktemp)
 
-# 2. shuffle the data
-cat temp_clean.txt | shuf > temp_shuffle.txt
+# 1. remove lines with only either :) or :( smiley and then shuffle data
+cat korp_all_sentences.txt | grep -Pv "^:\)$|^:\($" | shuf -o ${tempfile_clean}
 
-# 3. divide the data to training set (1/2) and test set (1/4) and a set from which to create evaluation set (1/4)
-len_total=$(wc -l temp_shuffle.txt| awk '{print $1}')
+# 2. divide the data to training set (1/2) and development set (1/4)
+# and a set from which to annotate an evaluation set (1/4)
 
-len_trainset=$(expr $len_total / 2)
-len_testset=$(expr $len_total - $len_trainset)
-len_evalset=$(expr $len_total - $len_trainset - $len_testset)
+len_total=$(wc -l $tempfile_clean| awk '{print $1}')
+len_half=$(expr $len_total / 2)
+len_quarter=$(expr $len_half / 2)
 
-cat temp_shuffle.txt | head -$len_trainset > temp_train.txt
-cat temp_shuffle.txt | tail -$len_testset > temp_rest.txt
-cat temp_rest.txt | head -$len_testset > temp_test.txt
-cat temp_rest.txt | tail -$len_evalset > temp_eval.txt
+# create training sets
+cat ${tempfile_clean} | head -${len_half} > ${tempfile_train}
 
-# 4. remove smileys and divide sets into negative and positive sentiment sets
-cat temp_train.txt |grep ":)"|perl -pe 's/\:\)//g' >  korp_train_pos.txt
-cat temp_train.txt |grep ":("|perl -pe 's/\:\(//g' >  korp_train_neg.txt
-cat temp_test.txt |grep ":)"|perl -pe 's/\:\)//g' >  korp_test_pos.txt
-cat temp_test.txt |grep ":("|perl -pe 's/\:\(//g' >  korp_test_neg.txt
+cat ${tempfile_train} | grep ":)"| perl -pe 's/\:\)//g' >  korp_train_pos.txt
+cat ${tempfile_train} | grep ":("| perl -pe 's/\:\(//g' >  korp_train_neg.txt
+
+# split rest into devel and eval
+cat ${tempfile_clean} | tail -${len_half} > ${tempfile_half}
+cat ${tempfile_half} | head -${len_quarter} > ${tempfile_devel}
+cat ${tempfile_half} | tail -${len_quarter} > ${tempfile_eval}
+
+# create development sets
+
+cat ${tempfile_devel} | grep ":)"| perl -pe 's/\:\)//g' >  korp_devel_pos.txt
+cat ${tempfile_devel} | grep ":("| perl -pe 's/\:\(//g' >  korp_devel_neg.txt
+
+# create raw data to be annotated as evaluation sets
+
+cat ${tempfile_eval} | perl -pe 's/\:\(//g' | perl -pe 's/\:\)//g' > korp_eval_raw.txt
+
+# 3. clean up
 
 # delete temporary files
-rm temp_*.txt
+rm ${tempfile_clean}
+rm ${tempfile_train}
+rm ${tempfile_half}
+rm ${tempfile_devel}
+rm ${tempfile_eval}
