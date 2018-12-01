638d65fb6c73b0a4c3d8064078d0ec6580cd807e
tjkemp <tero.kemppi@gmail.com>
2018-12-01 23:31:16 +0200

    evaluate and print confusion matrix

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,39 +1,45 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 from sklearn.externals import joblib
 import codecs
 
-def load_sentences(dataset='Turku'):
+def load_data(classes=['pos', 'neg'], dataset='unknown'):
     sentences = []
-    lan_sentences = codecs.open('./cities/%s.txt' % (dataset), 'r', 'utf-8').readlines()
-    sentences += [s.strip() for s in lan_sentences]
+    labels = []
+    for class_ in classes:
+        filename = './data/%s_%s.txt' % (dataset, class_)
+        lan_sentences = codecs.open(filename, 'r', 'utf-8').readlines()
+        sentences += [s.strip() for s in lan_sentences]
+        labels += [class_] * len(lan_sentences)
 
-    return sentences
+    return sentences, labels
+
+def print_cm(matrix, labels):
+    print()
+    print("Confusion matrix:")
+    print(f"\t{labels[0]}\t{labels[1]}")
+    print(f"{labels[0]}\t{matrix[0][0]}\t{matrix[0][1]}") 
+    print(f"{labels[1]}\t{matrix[1][0]}\t{matrix[1][1]}") 
 
 def main():
-    #
-    # NOTE: this file is under development
-    #
 
     # load previously generated vectorizer and classifier
     vectorizer = joblib.load('model/vectorizer.joblib')
     classifier = joblib.load('model/classifier.joblib')
 
     # load sentences to predict
-    datasets = ['Rauma', 'Kotka', 'Lappeenranta', 'Rovaniemi']
+    eval_sentences, eval_labels = load_data(dataset='korp_devel')
+    predict_vectors = vectorizer.transform(eval_sentences).toarray()
+    predictions = classifier.predict(predict_vectors)
+    score_eval = accuracy_score(eval_labels, predictions)
 
-    for dataset in datasets:
-        predict_sentences = load_sentences(dataset=dataset)
+    print(f"On the dataset the model has the accuracy of {score_eval:.3f}.")
 
-        predict_vectors = vectorizer.transform(predict_sentences)
-
-        predictions = classifier.predict(predict_vectors)
-        pred_pos = (predictions == 'pos').sum()
-        pred_neg = (predictions == 'neg').sum()
-
-        print("Dataset {} has {} positive and {} negative sentences with positivity % of {:.2f}.").format(
-            dataset, pred_pos, pred_neg, (float(pred_pos) / (pred_pos+pred_neg))*100)
+    labels = ["pos", "neg"]
+    cm = confusion_matrix(eval_labels, predictions, labels=labels)
+    print_cm(cm, labels)
 
 if __name__ == "__main__":
     main()
