f4a4a274c24248773df3d40a425517f5d1724fd1
tjkemp <tero.kemppi@gmail.com>
2018-12-01 18:53:27 +0200

    several updates

--- train_classifier.py
+++ train_classifier.py
@@ -4,22 +4,26 @@ from sklearn.metrics import accuracy_score
 from sklearn.externals import joblib
 import codecs
 
-def load_data(classes=['pos', 'neg'], dataset='unknown'):
+def load_data(classes=['pos', 'neg'], dataset='unknown', limit=1000):
     sentences = []
     labels = []
-    for c in classes:
-        lan_sentences = codecs.open('./data/%s_%s.txt' % (dataset, c), 'r', 'utf-8').readlines()
-        sentences += [s.strip() for s in lan_sentences]
-        labels += [c] * len(lan_sentences)
+    for class_ in classes:
+        filename = './data/%s_%s.txt' % (dataset, class_)
+        lan_sentences = codecs.open(filename, 'r', 'utf-8').readlines()
+        if len(lan_sentences) > limit:
+            print(f"Warning: enforcing limit of {limit} samples to dataset {filename}")
+        sentences += [s.strip() for s in lan_sentences[:limit]]
+        label_count = len(lan_sentences) if len(lan_sentences) < limit else limit
+        labels += [class_] * label_count
 
     return sentences, labels
 
 def main():
     # load data sets
-    # big datasets: korp_train and korp_test
-    # small datasets: small_train and small_test
-    train_sentences, train_labels = load_data(dataset='small_train')
-    test_sentences, test_labels = load_data(dataset='small_test')
+    train_sentences, train_labels = load_data(dataset='korp_train')
+    test_sentences, test_labels = load_data(dataset='korp_devel')
+
+    print("Found {} training sentences, {} test sentences.".format(len(train_sentences), len(test_sentences)))
 
     # convert sentences to feature vectors
     print ("Creating a vectorizer...")
@@ -40,20 +44,19 @@ def main():
         c = 2**i
         classifier = LinearSVC(C=c)
         classifier.fit(train_vectors, train_labels)
-        score = accuracy_score(test_labels, classifier.predict(test_vectors))
-        if score > score_best:
+        score_train = accuracy_score(train_labels, classifier.predict(train_vectors))
+        score_test = accuracy_score(test_labels, classifier.predict(test_vectors))
+        if score_test > score_best:
             c_best = c
-            score_best = score
+            score_best = score_test
             classifier_best = classifier
-        print ("With C = %s test set accuracy: %s." % (c, score))
+        print ("With C = %s with test set accuracy: %s (and training set accuracy: %s)." % (c, score_test, score_train))
 
-    print ("Best C value is %s with the accuracy of %s." % (c_best,score_best))
+    print ("Best C value is %s with the accuracy of %s." % (c_best, score_best))
 
     # save the best classifier
     print ("Saving vectorizer and classifier...")
-    #with open('model/vectorizer.joblib', 'wb') as f:
     joblib.dump(vectorizer, 'model/vectorizer.joblib')
-    #with open('model/classifier.joblib', 'wb') as f:
     joblib.dump(classifier_best, 'model/classifier.joblib')
     print ("Done.")
 
