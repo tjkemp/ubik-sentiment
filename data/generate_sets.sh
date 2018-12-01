7af4d8b01a1720afe9ea33efdaf2b19c91124b19
tjkemp <tero.kemppi@gmail.com>
2018-12-01 08:57:10 +0200

    renamed to describe it better

--- data/generate_sets.sh
+++ data/generate_sets.sh
@@ -9,14 +9,17 @@ cat korp_lauseet.txt | grep -Pv "^:\)$|^:\($" > temp_clean.txt
 # 2. shuffle the data
 cat temp_clean.txt | shuf > temp_shuffle.txt
 
-# 3. divide the data to training set (2/3) and test set (1/3)
+# 3. divide the data to training set (1/2) and test set (1/4) and a set from which to create evaluation set (1/4)
 len_total=$(wc -l temp_shuffle.txt| awk '{print $1}')
 
-len_testset=$(expr $len_total / 3)
-len_trainset=$(expr $len_total - $len_testset)
+len_trainset=$(expr $len_total / 2)
+len_testset=$(expr $len_total - $len_trainset)
+len_evalset=$(expr $len_total - $len_trainset - $len_testset)
 
 cat temp_shuffle.txt | head -$len_trainset > temp_train.txt
-cat temp_shuffle.txt | tail -$len_testset > temp_test.txt
+cat temp_shuffle.txt | tail -$len_testset > temp_rest.txt
+cat temp_rest.txt | head -$len_testset > temp_test.txt
+cat temp_rest.txt | tail -$len_evalset > temp_eval.txt
 
 # 4. remove smileys and divide sets into negative and positive sentiment sets
 cat temp_train.txt |grep ":)"|perl -pe 's/\:\)//g' >  korp_train_pos.txt
